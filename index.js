54bd297975d76ec5ab657394d9ebd7d39e080153
BopItFreak <augustberchelmann@gmail.com>
2019-01-16 19:28:46 -0500

    add stuff

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 global.config = require("./config.json");
 
 var MPPclient = require("mpp-client");
-global.client = new MPPclient("ws://www.multiplayerpiano.com:443");
+global.client = new MPPclient("ws://proxys4.herokuapp.com");
 client.setChannel("lobby");
 client.start();
 client.on("hi", function(){
   client.sendArray([{m:'userset', set: {name: config.botName}}]);
+});
+client.on("p",function(msg) {
+  msg._id === client.getOwnParticipant()._id ? client.sendArray([{m:'userset', set: {name: "Bop It"}}]) : {};
 });
 
 client.on("a", require("./commandHandler"));
